33342032f5efe358b01df593ca3049d9dcb91079
Ricardo Machado <r.machado@elsevier.com>
2020-11-10 17:59:30 +0100

    Updates

--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('test')
+  document.getElementById('root')
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
